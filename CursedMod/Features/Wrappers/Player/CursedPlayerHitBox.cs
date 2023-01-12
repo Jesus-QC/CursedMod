411bc7158cd48b94f0890644d0ce4c941687608d
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-12 17:03:45 +0100

    `CursedPlayerHitBox`

---
+++ CursedMod/Features/Wrappers/Player/CursedPlayerHitBox.cs
@@ -0,0 +1,23 @@
+﻿using PlayerStatsSystem;
+using UnityEngine;
+
+namespace CursedMod.Features.Wrappers.Player;
+
+public class CursedPlayerHitBox
+{
+    public HitboxIdentity Base { get; }
+
+    public CursedPlayer Owner => CursedPlayer.Get(Base.TargetHub);
+
+    public HitboxType HitBoxType
+    {
+        get => Base.HitboxType;
+        set => Base._dmgMultiplier = value;
+    }
+
+    public bool Damage(float damage, DamageHandlerBase handler, Vector3 exactPosition) => Base.Damage(damage, handler, exactPosition);
+
+    public void DisableColliders() => Base.SetColliders(false);
+    
+    public void EnableColliders() => Base.SetColliders(true);
+}
