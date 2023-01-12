5a10c3651791a4dc6504adfe3bc964afe2c5c314
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-12 16:33:59 +0100

    fix fix

--- CursedMod/Features/Wrappers/Facility/CursedGenerator.cs
+++ CursedMod/Features/Wrappers/Facility/CursedGenerator.cs
@@ -5,39 +5,39 @@ using UnityEngine;
 
 namespace CursedMod.Features.Wrappers.Facility;
 
-public static class CursedGenerator
+public class CursedGenerator
 {
-    public static Scp079Generator Base { get; private set; } // todo: constructor
+    public Scp079Generator Base { get; private set; } // todo: constructor
 
-    public static bool IsEngaged
+    public bool IsEngaged
     {
         get => Base.Engaged;
         set => Base.Engaged = value;
     }
 
-    public static bool IsActivating
+    public bool IsActivating
     {
         get => Base.Activating;
         set => Base.Activating = value;
     }
 
-    public static int RemainingTime => Base.RemainingTime;
+    public int RemainingTime => Base.RemainingTime;
     
-    public static float DropdownSpeed => Base.DropdownSpeed;
+    public float DropdownSpeed => Base.DropdownSpeed;
     
-    public static Vector3 Position => Base.transform.position; //todo: setter
+    public Vector3 Position => Base.transform.position; //todo: setter
     
-    public static Quaternion Rotation => Base.transform.rotation; // todo: setter
+    public Quaternion Rotation => Base.transform.rotation; // todo: setter
     
-    public static Vector3 Scale => Base.transform.localScale; // todo: setter
+    public Vector3 Scale => Base.transform.localScale; // todo: setter
 
-    public static KeycardPermissions RequiredPermissions
+    public KeycardPermissions RequiredPermissions
     {
         get => Base._requiredPermission;
         set => Base._requiredPermission = value;
     }
 
-    public static NetworkIdentity NetworkIdentity => Base.netIdentity;
+    public NetworkIdentity NetworkIdentity => Base.netIdentity;
     
-    public static string ToString() => $"{nameof(CursedGenerator)}: Engaged {IsEngaged} | Activating: {IsActivating} | Remaining Time: {RemainingTime} | DropdownSpeed: {DropdownSpeed} | Position: {Position} | Rotation: {Rotation} | Scale: {Scale} | Permissions: {RequiredPermissions} | NetId: {NetworkIdentity.netId}";
+    public override string ToString() => $"{nameof(CursedGenerator)}: Engaged {IsEngaged} | Activating: {IsActivating} | Remaining Time: {RemainingTime} | DropdownSpeed: {DropdownSpeed} | Position: {Position} | Rotation: {Rotation} | Scale: {Scale} | Permissions: {RequiredPermissions} | NetId: {NetworkIdentity.netId}";
 }
