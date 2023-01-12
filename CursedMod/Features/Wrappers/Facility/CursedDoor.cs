3cfa7c1496e9f30397786f88a15620f5247da8c9
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-01-12 22:08:46 +0100

    More Generator and Door API and added CursedCamera

--- CursedMod/Features/Wrappers/Facility/CursedDoor.cs
+++ CursedMod/Features/Wrappers/Facility/CursedDoor.cs
@@ -1,4 +1,5 @@
-﻿using Interactables.Interobjects.DoorUtils;
+﻿using Interactables.Interobjects;
+using Interactables.Interobjects.DoorUtils;
 using UnityEngine;
 
 namespace CursedMod.Features.Wrappers.Facility;
@@ -17,6 +18,14 @@ public class CursedDoor
     public Transform Transform => Base.transform;
 
     public int DoorId => Base.DoorId;
+    
+    public float State => Base.GetExactState();
+    
+    public bool IsMoving => State is not(0 or 1);
+    
+    public bool FullClosed => State is 0;
+    
+    public bool FullOpened => State is 1;
 
     public Vector3 Position
     {
@@ -44,13 +53,34 @@ public class CursedDoor
 
     public bool IsOpen
     {
-        get => Base.TargetState;
+        get => Base.IsConsideredOpen();
         set => Base.NetworkTargetState = value;
     }
+    
+    public bool IsGate => Base is PryableDoor;
+    
+    public bool IsCheckpoint => Base is CheckpointDoor;
+    
+    public bool IsElevator => Base is ElevatorDoor;
+    
+    public bool IsBroken => Base is IDamageableDoor damageable && damageable.IsDestroyed;
+    
+    public DoorNametagExtension Tag => Base.GetComponent<DoorNametagExtension>();
 
     public void TriggerState() => Base.NetworkTargetState = !IsOpen;
 
     public void ServerChangeLock(DoorLockReason reason, bool newState) => Base.ServerChangeLock(reason, newState);
+    
+    public bool DestroyDoor(DoorDamageType type = DoorDamageType.ServerCommand)
+    {
+        if (Base is IDamageableDoor damageable && !damageable.IsDestroyed)
+        {
+            damageable.ServerDamage(ushort.MaxValue, type);
+            return true;
+        }
+
+        return false;
+    }
 
     public override string ToString() =>
         $"{nameof(CursedDoor)}: Opened: {IsOpen} | Position: {Position} | Rotation: {Rotation} | Scale: {Scale} | Permissions: {RequiredPermissions} | DoorId: {DoorId}";
--- CursedMod/Features/Wrappers/Facility/CursedGenerator.cs
+++ CursedMod/Features/Wrappers/Facility/CursedGenerator.cs
@@ -13,6 +13,22 @@ public class CursedGenerator
     {
         Base = generator;
     }
+    
+    public GameObject GameObject => Base.gameObject;
+
+    public bool ReadyToActivate => Base.ActivationReady;
+    
+    public bool Opened
+    {
+        get => Base.HasFlag(Base.Network_flags, Scp079Generator.GeneratorFlags.Open);
+        set => Base.ServerSetFlag(Scp079Generator.GeneratorFlags.Unlocked, value);
+    }
+    
+    public bool IsUnlocked
+    {
+        get => Base.HasFlag(Base.Network_flags, Scp079Generator.GeneratorFlags.Unlocked);
+        set => Base.ServerSetFlag(Scp079Generator.GeneratorFlags.Unlocked, value);
+    }
 
     public bool IsEngaged
     {
@@ -41,6 +57,8 @@ public class CursedGenerator
         get => Base.transform.rotation;
         set => Base.transform.rotation = value;
     }
+    
+    public Transform Transform => Base.transform;
 
     public Vector3 Scale
     {
@@ -54,8 +72,17 @@ public class CursedGenerator
         set => Base._requiredPermission = value;
     }
 
+    public void SetPermission(KeycardPermissions keycard, bool isEnabled)
+    {
+        KeycardPermissions permission = keycard;
+        if (isEnabled)
+            Base._requiredPermission |= permission;
+        else
+            Base._requiredPermission &= ~permission;
+    }
+
     public NetworkIdentity NetworkIdentity => Base.netIdentity;
 
     public override string ToString() =>
-        $"{nameof(CursedGenerator)}: Engaged {IsEngaged} | Activating: {IsActivating} | Remaining Time: {RemainingTime} | DropdownSpeed: {DropdownSpeed} | Position: {Position} | Rotation: {Rotation} | Scale: {Scale} | Permissions: {RequiredPermissions} | NetId: {NetworkIdentity.netId}";
+        $"{nameof(CursedGenerator)}: Opened: {Opened} | Engaged {IsEngaged} | Activating: {IsActivating} | Remaining Time: {RemainingTime} | DropdownSpeed: {DropdownSpeed} | Position: {Position} | Rotation: {Rotation} | Scale: {Scale} | Permissions: {RequiredPermissions} | NetId: {NetworkIdentity.netId}";
 }
