e58fe5b1b67333fbf4b574f3fa037790651a313a
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-01-13 17:04:46 +0100

    Fix for CursedRoom

--- CursedMod/Features/Wrappers/Facility/Rooms/CursedRoom.cs
+++ CursedMod/Features/Wrappers/Facility/Rooms/CursedRoom.cs
@@ -6,6 +6,11 @@ namespace CursedMod.Features.Wrappers.Facility.Rooms;
 public class CursedRoom
 {
     public RoomIdentifier Room { get; }
+    
+    public CursedRoom(RoomIdentifier room)
+    {
+        Room = room;
+    }
 
     public Vector3 Position => Room.transform.position;
     
