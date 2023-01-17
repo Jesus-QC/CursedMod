5b1280c297ac2ecd58318ff49a5e926c3fa99946
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-17 19:17:55 +0100

    rename

---
+++ CursedMod/Features/Wrappers/Player/RagDolls/CursedRagDoll.cs
@@ -0,0 +1,35 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace CursedMod.Features.Wrappers.Player.Ragdolls;
+
+public class CursedRagDoll
+{
+    public BasicRagdoll RagDollBase { get; }
+    public Transform Transform { get; }
+    public GameObject GameObject { get; }
+
+    public bool AutoCleanUp
+    {
+        get => !RagDollBase._cleanedUp;
+        set => RagDollBase._cleanedUp = !value;
+    }
+
+    public RagdollData Data
+    {
+        get => RagDollBase.Info;
+        set => RagDollBase.NetworkInfo = value;
+    }
+
+    public void CleanUp() => RagDollBase.OnCleanup();
+
+    public void Destroy() => NetworkServer.Destroy(GameObject);
+    
+    
+    public CursedRagDoll(BasicRagdoll ragDoll)
+    {
+        RagDollBase = ragDoll;
+        Transform = ragDoll.transform;
+        GameObject = ragDoll.gameObject;
+    }
+}
