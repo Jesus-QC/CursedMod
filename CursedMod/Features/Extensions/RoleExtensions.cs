b5f10321b055cbaf91a7d650e59248ac0ae8430c
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-18 23:17:46 +0100

    `CursedRoleManager`

---
+++ CursedMod/Features/Extensions/RoleExtensions.cs
@@ -0,0 +1,27 @@
+﻿using PlayerRoles;
+using PlayerRoles.FirstPersonControl;
+using PlayerRoles.FirstPersonControl.Spawnpoints;
+using UnityEngine;
+
+namespace CursedMod.Features.Extensions;
+
+public static class RoleExtensions
+{
+    public static Vector3 GetRandomSpawnPosition(this RoleTypeId roleType)
+    {
+        if(!PlayerRoleLoader.TryGetRoleTemplate(roleType, out PlayerRoleBase roleBase))
+            return Vector3.zero;
+            
+        if(roleBase is not IFpcRole fpc)
+            return Vector3.zero;
+
+        ISpawnpointHandler spawn = fpc.SpawnpointHandler;
+        if(spawn is null)
+            return Vector3.zero;
+
+        if (spawn.TryGetSpawnpoint(out Vector3 pos, out float _))
+            return pos;
+
+        return Vector3.zero;
+    }
+}
---
+++ CursedMod/Features/Wrappers/Player/Roles/CursedRoleManager.cs
@@ -0,0 +1,10 @@
+﻿using CursedMod.Features.Extensions;
+using PlayerRoles;
+using UnityEngine;
+
+namespace CursedMod.Features.Wrappers.Player.Roles;
+
+public static class CursedRoleManager
+{
+    public static Vector3 GetRoleSpawnPosition(RoleTypeId role) => role.GetRandomSpawnPosition();
+}
