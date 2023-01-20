b0f47f30c60b2d8c8ddc87754ac9e21143e262e2
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-20 17:32:46 +0100

    KeyCard and MicroHid

---
+++ CursedMod/Features/Wrappers/Inventory/Items/KeyCards/CursedKeyCard.cs
@@ -0,0 +1,21 @@
+﻿using Interactables.Interobjects.DoorUtils;
+using InventorySystem.Items;
+using InventorySystem.Items.Keycards;
+
+namespace CursedMod.Features.Wrappers.Inventory.Items.Keycards;
+
+public class CursedKeyCard : CursedItem
+{
+    public KeycardItem KeyCardBase { get; }
+    
+    internal CursedKeyCard(KeycardItem itemBase) : base(itemBase)
+    {
+        KeyCardBase = itemBase;
+    }
+
+    public KeycardPermissions Permissions
+    {
+        get => KeyCardBase.Permissions;
+        set => KeyCardBase.Permissions = value;
+    }
+}
---
+++ CursedMod/Features/Wrappers/Inventory/Items/MicroHID/CursedMicroHID.cs
@@ -0,0 +1,44 @@
+﻿using InventorySystem.Items;
+using InventorySystem.Items.MicroHID;
+
+namespace CursedMod.Features.Wrappers.Inventory.Items.MicroHID;
+
+public class CursedMicroHid : CursedItem
+{
+    public MicroHIDItem MicroHidBase { get; }
+    
+    internal CursedMicroHid(MicroHIDItem itemBase) : base(itemBase)
+    {
+        MicroHidBase = itemBase;
+    }
+
+    public float Readiness => MicroHidBase.Readiness;
+
+    public byte EnergyToByte => MicroHidBase.EnergyToByte;
+
+    public void Recharge() => MicroHidBase.Recharge();
+
+    public void SendStatus(HidStatusMessageType status, byte code) => MicroHidBase.ServerSendStatus(status, code);
+
+    public void Fire() => MicroHidBase.Fire();
+
+    public float RemainingEnergy
+    {
+        get => MicroHidBase.RemainingEnergy;
+        set
+        {
+            MicroHidBase.RemainingEnergy = value;
+            SendStatus(HidStatusMessageType.EnergySync, EnergyToByte);
+        }
+    }
+
+    public HidState State
+    {
+        get => MicroHidBase.State;
+        set
+        {
+            MicroHidBase.State = value;
+            MicroHidBase.ServerSendStatus(HidStatusMessageType.State, (byte)value);
+        }
+    }
+}
