1c21cf9f8de0e6e829ac7c4c42ba4cda331d6410
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-20 17:20:09 +0100

    Jailbird and flashlight

---
+++ CursedMod/Features/Wrappers/Inventory/Items/Flashlight/CursedFlashlight.cs
@@ -0,0 +1,25 @@
+﻿using InventorySystem.Items;
+using InventorySystem.Items.Flashlight;
+using Utils.Networking;
+
+namespace CursedMod.Features.Wrappers.Inventory.Items.Flashlight;
+
+public class CursedFlashlight : CursedItem
+{
+    public FlashlightItem FlashlightBase { get; }
+    
+    internal CursedFlashlight(FlashlightItem itemBase) : base(itemBase)
+    {
+        FlashlightBase = itemBase;
+    }
+
+    public bool IsEmittingLight
+    {
+        get => FlashlightBase.IsEmittingLight;
+        set
+        {
+            FlashlightBase.IsEmittingLight = value;
+            new FlashlightNetworkHandler.FlashlightMessage(Base.ItemSerial, value).SendToAuthenticated();
+        }
+    }
+}
---
+++ CursedMod/Features/Wrappers/Inventory/Items/Jailbird/CursedJailbird.cs
@@ -0,0 +1,72 @@
+﻿using InventorySystem.Items;
+using InventorySystem.Items.Jailbird;
+
+namespace CursedMod.Features.Wrappers.Inventory.Items.Jailbird;
+
+public class CursedJailbird : CursedItem
+{
+    public JailbirdItem JailbirdBase { get; }
+    
+    internal CursedJailbird(JailbirdItem itemBase) : base(itemBase)
+    {
+        JailbirdBase = itemBase;
+    }
+
+    public int TotalChargesPerformed
+    {
+        get => JailbirdBase.TotalChargesPerformed;
+        set => JailbirdBase.TotalChargesPerformed = value;
+    }
+
+    public void Attack() => JailbirdBase.ServerAttack(null);
+
+    public JailbirdHitreg HitRegion => JailbirdBase._hitreg;
+
+    public double ChargeResetTime
+    {
+        get => JailbirdBase._chargeResetTime;
+        set => JailbirdBase._chargeResetTime = value;
+    }
+
+    public bool ChargeLoading
+    {
+        get => JailbirdBase._chargeLoading;
+        set => JailbirdBase._chargeLoading = value;
+    }
+
+    public bool Charging
+    {
+        get => JailbirdBase._charging;
+        set => JailbirdBase._charging = value;
+    }
+
+    public float MeleeDelay
+    {
+        get => JailbirdBase._meleeDelay;
+        set => JailbirdBase._meleeDelay = value;
+    }
+    
+    public float MeleeCooldown
+    {
+        get => JailbirdBase._meleeCooldown;
+        set => JailbirdBase._meleeCooldown = value;
+    }
+
+    public float ChargeDuration
+    {
+        get => JailbirdBase._chargeDuration;
+        set => JailbirdBase._chargeDuration = value;
+    }
+
+    public float ChargeMovementMultiplier
+    {
+        get => JailbirdBase._chargeMovementSpeedMultiplier;
+        set => JailbirdBase._chargeMovementSpeedMultiplier = value;
+    }
+
+    public float ChargeMovementLimiter
+    {
+        get => JailbirdBase._chargeMovementSpeedLimiter;
+        set => JailbirdBase._chargeMovementSpeedLimiter = value;
+    }
+}
