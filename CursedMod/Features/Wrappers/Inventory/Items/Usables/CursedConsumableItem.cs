5ad9c4f6cc3887f03eac08f4cdf26c9e77567893
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-22 20:06:40 +0100

    some consumables

---
+++ CursedMod/Features/Wrappers/Inventory/Items/Usables/CursedConsumableItem.cs
@@ -0,0 +1,17 @@
+﻿using InventorySystem.Items.Usables;
+
+namespace CursedMod.Features.Wrappers.Inventory.Items.Usables;
+
+public class CursedConsumableItem : CursedUsableItem
+{
+    public Consumable BaseConsumable { get; }
+    
+    internal CursedConsumableItem(Consumable itemBase) : base(itemBase)
+    {
+        BaseConsumable = itemBase;
+    }
+
+    public bool ActivationReady => BaseConsumable.ActivationReady;
+
+    public void Consume() => BaseConsumable.ActivateEffects();
+}
---
+++ CursedMod/Features/Wrappers/Inventory/Items/Usables/CursedUsableItem.cs
@@ -0,0 +1,48 @@
+using InventorySystem.Items;
+using InventorySystem.Items.Usables;
+
+namespace CursedMod.Features.Wrappers.Inventory.Items.Usables;
+
+public class CursedUsableItem : CursedItem
+{
+    public UsableItem UsableBase { get; }
+    
+    internal CursedUsableItem(UsableItem itemBase) : base(itemBase)
+    {
+        UsableBase = itemBase;
+    }
+
+    public bool CanStartUsing
+    {
+        get => UsableBase.CanStartUsing;
+        set => UsableBase.CanStartUsing = value;
+    }
+
+    public bool IsUsing
+    {
+        get => UsableBase.IsUsing;
+        set => UsableBase.IsUsing = value;
+    }
+
+    public void SetPersonalCooldown(float seconds) => UsableBase.ServerSetPersonalCooldown(seconds);
+    
+    public void SetGlobalCooldown(float seconds) => UsableBase.ServerSetGlobalItemCooldown(seconds);
+
+    public float RemainingCooldown
+    {
+        get => UsableBase.RemainingCooldown;
+        set => UsableBase.RemainingCooldown = value;
+    }
+
+    public float UseTime
+    {
+        get => UsableBase.UseTime;
+        set => UsableBase.UseTime = value;
+    }
+
+    public float MaxCancellableTime
+    {
+        get => UsableBase.MaxCancellableTime;
+        set => UsableBase.MaxCancellableTime = value;
+    }
+}
