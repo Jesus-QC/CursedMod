ac236be06389b532e05cd6b2ff5f72963676766c
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-01-23 22:13:24 +0100

    VoiceChat

---
+++ CursedMod/Features/Wrappers/Player/VoiceChat/CursedVoiceChat.cs
@@ -0,0 +1,30 @@
+﻿using PlayerRoles.Voice;
+using VoiceChat;
+
+namespace CursedMod.Features.Wrappers.Player.VoiceChat;
+
+public class CursedVoiceChat
+{
+    public VoiceModuleBase VoiceModule { get; }
+
+    public CursedVoiceChat(VoiceModuleBase voiceModule)
+    {
+        VoiceModule = voiceModule;
+    }
+
+    public VoiceChatChannel LastChannel => VoiceModule._lastChannel;
+
+    public VoiceChatChannel CurrentChannel
+    {
+        get => VoiceModule.CurrentChannel;
+        set => VoiceModule.CurrentChannel = value;
+    }
+
+    public bool IsSpeaking => VoiceModule.IsSpeaking;
+
+    public void CheckRateLimit() => VoiceModule.CheckRateLimit();
+
+    public void ValidateReceive(ReferenceHub refHub, VoiceChatChannel channel) => VoiceModule.ValidateReceive(refHub, channel);
+    
+    public void ValidateSend(VoiceChatChannel channel) => VoiceModule.ValidateSend(channel);
+}
---
+++ CursedMod/Features/Wrappers/Player/VoiceChat/CursedVoicePlayback.cs
@@ -0,0 +1,25 @@
+﻿using VoiceChat.Playbacks;
+
+namespace CursedMod.Features.Wrappers.Player.VoiceChat;
+
+public class CursedVoicePlayback
+{
+    public VoiceChatPlaybackBase VoiceChatPlayback { get; }
+    
+    public CursedVoicePlayback(VoiceChatPlaybackBase voiceChatPlayback)
+    {
+        VoiceChatPlayback = voiceChatPlayback;
+    }
+    
+    public float Loudness
+    {
+        get => VoiceChatPlayback.Loudness;
+        set => VoiceChatPlayback.Loudness = value;
+    }
+
+    public float VolumeScale
+    {
+        get => VoiceChatPlayback.VolumeScale;
+        set => VoiceChatPlayback.VolumeScale = value;
+    }
+}
