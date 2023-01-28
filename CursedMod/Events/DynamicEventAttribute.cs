44e2333f99c2fd5a30582c5fd6e123e2d352aeef
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-28 14:17:45 +0100

    added da DynamicEventAttribute.cs for the future

---
+++ CursedMod/Events/DynamicEventAttribute.cs
@@ -0,0 +1,14 @@
+﻿/*using System;
+using System.Reflection;
+
+namespace CursedMod.Events;
+
+public class DynamicEventAttribute : Attribute
+{
+    public EventInfo Method;
+
+    public DynamicEventAttribute(Type type, string name)
+    {
+        Method = type.GetEvent(name);
+    }
+}*/
