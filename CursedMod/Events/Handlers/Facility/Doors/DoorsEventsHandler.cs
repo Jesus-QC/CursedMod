1894a7fface5f8d0e9a0c50255ca2e7187fc23f9
Jesus-QC <jesusquirocampos@gmail.com>
2023-03-08 19:40:01 +0100

    PlayerInteractingDoor

---
+++ CursedMod/Events/Handlers/Facility/Doors/DoorsEventsHandler.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DoorsEventsHandler.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using CursedMod.Events.Arguments.Facility.Doors;
+
+namespace CursedMod.Events.Handlers.Facility.Doors;
+
+public static class DoorsEventsHandler
+{
+    public static event EventManager.CursedEventHandler<PlayerInteractingDoorEventArgs> PlayerInteractingDoor;
+
+    public static void OnPlayerInteractingDoor(PlayerInteractingDoorEventArgs args)
+    {
+       PlayerInteractingDoor.InvokeEvent(args); 
+    }
+}
---
+++ CursedMod/Events/Patches/Facility/Doors/ServerInteractDoorVariantPatch.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ServerInteractDoorVariantPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.Facility.Doors;
+using CursedMod.Events.Handlers.Facility.Doors;
+using HarmonyLib;
+using Interactables.Interobjects.DoorUtils;
+using NorthwoodLib.Pools;
+
+namespace CursedMod.Events.Patches.Facility.Doors;
+
+[HarmonyPatch(typeof(DoorVariant), nameof(DoorVariant.ServerInteract))]
+public class ServerInteractDoorVariantPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<ServerInteractDoorVariantPatch>(116, instructions);
+
+        LocalBuilder args = generator.DeclareLocal(typeof(PlayerInteractingDoorEventArgs));
+        Label ret = generator.DefineLabel();
+            
+        newInstructions[newInstructions.Count - 1].labels.Add(ret);
+        
+        int offset = newInstructions.FindIndex(x => x.opcode == OpCodes.Beq_S) + 11;
+        
+        newInstructions.InsertRange(offset, new CodeInstruction[]
+        {
+            new (OpCodes.Ldarg_0),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Ldloc_0),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerInteractingDoorEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Stloc_S, args.LocalIndex),
+            new (OpCodes.Call, AccessTools.Method(typeof(DoorsEventsHandler), nameof(DoorsEventsHandler.OnPlayerInteractingDoor))),
+            new (OpCodes.Ldloc_S, args.LocalIndex),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerInteractingDoorEventArgs), nameof(PlayerInteractingDoorEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, ret),
+            new (OpCodes.Ldloc_S, args.LocalIndex),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerInteractingDoorEventArgs), nameof(PlayerInteractingDoorEventArgs.HasPermissions))),
+            new (OpCodes.Stloc_0),
+        });
+        
+        foreach (CodeInstruction instruction in newInstructions)
+            yield return instruction;
+
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
