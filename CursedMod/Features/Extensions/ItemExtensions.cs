8cef58271369e6c70b895f1b4e9fb8750f408f72
Jesus-QC <jesusquirocampos@gmail.com>
2023-03-10 19:22:48 +0100

    ItemExtensions

---
+++ CursedMod/Features/Extensions/ItemExtensions.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ItemExtensions.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace CursedMod.Features.Extensions;
+
+public static class ItemExtensions
+{
+    public static bool IsKeyCard(this ItemType itemType)
+    {
+        int i = (int)itemType;
+        return i is > -1 and < 12;
+    }
+}
