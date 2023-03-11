704a0fef560f221340803cb0b477d6298c99c260
Jesus-QC <jesusquirocampos@gmail.com>
2023-03-11 23:02:29 +0100

    RespawningEventsHandler::OnRespawningTeam

---
+++ CursedMod/Events/Arguments/Respawning/RespawningTeamEventArgs.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RespawningTeamEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using Respawning;
+
+namespace CursedMod.Events.Arguments.Respawning;
+
+public class RespawningTeamEventArgs : EventArgs, ICursedCancellableEvent
+{
+    public RespawningTeamEventArgs()
+    {
+        IsAllowed = true;
+    }
+    
+    public bool IsAllowed { get; set; }
+    
+    public SpawnableTeamType TeamSpawning
+    {
+        get => RespawnManager.Singleton.NextKnownTeam;
+        set => RespawnManager.Singleton.NextKnownTeam = value;
+    }
+}
---
+++ CursedMod/Events/Handlers/Respawning/RespawningEventsHandler.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RespawningEventsHandler.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using CursedMod.Events.Arguments.Respawning;
+
+namespace CursedMod.Events.Handlers.Respawning;
+
+public static class RespawningEventsHandler
+{
+    public static event EventManager.CursedEventHandler<RespawningTeamEventArgs> RespawningTeam;
+
+    internal static void OnRespawningTeam(RespawningTeamEventArgs args)
+    {
+        RespawningTeam.InvokeEvent(args);
+    }
+}
