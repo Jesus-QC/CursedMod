b6d04835dc1a922c06554f5694a5332b09590221
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-06 05:42:57 +0200

    Implements ICursedRagdollEvent and Reviving (049)

---
+++ CursedMod/Events/Handlers/SCPs/Scp049/Scp049EventsHandler.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Scp049EventsHandler.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using CursedMod.Events.Arguments.SCPs.Scp049;
+
+namespace CursedMod.Events.Handlers.SCPs.Scp049;
+
+public static class Scp049EventsHandler
+{
+    public static event EventManager.CursedEventHandler<PlayerRevivingEventArgs> PlayerReviving;
+    
+    internal static void OnPlayerReviving(PlayerRevivingEventArgs args)
+    {
+        PlayerReviving.InvokeEvent(args);
+    }
+}
