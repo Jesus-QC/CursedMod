316ff328b39014cfdcb662be9e6e6350000c4dd5
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-07 23:06:24 +0200

    Scp939 Handlers and SavingVoice Patch

---
+++ CursedMod/Events/Handlers/SCPs/Scp939/Scp939EventsHandler.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Scp939EventsHandler.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using CursedMod.Events.Arguments.SCPs.Scp939;
+
+namespace CursedMod.Events.Handlers.SCPs.Scp939;
+
+public static class Scp939EventsHandler
+{
+    public static event EventManager.CursedEventHandler<PlayerSavingVoiceEventArgs> PlayerSavingVoice;
+    
+    public static void OnPlayerSavingVoice(PlayerSavingVoiceEventArgs args)
+    {
+        PlayerSavingVoice.InvokeEvent(args);
+    }
+}
---
+++ CursedMod/Events/Patches/SCPs/Scp939/SaveVoicePatch.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SaveVoicePatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp939;
+using CursedMod.Events.Handlers.SCPs.Scp939;
+using CursedMod.Features.Wrappers.Player;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp939.Mimicry;
+
+namespace CursedMod.Events.Patches.SCPs.Scp939;
+
+[DynamicEventPatch(typeof(Scp939EventsHandler), nameof(Scp939EventsHandler.PlayerSavingVoice))]
+[HarmonyPatch(typeof(MimicryRecorder), nameof(MimicryRecorder.OnAnyPlayerKilled))]
+public class SaveVoicePatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<SaveVoicePatch>(53, instructions);
+
+        Label retLabel = generator.DefineLabel();
+
+        const int offset = 3;
+        int index = newInstructions.FindIndex(i =>
+            i.Calls(AccessTools.Method(typeof(MimicryRecorder), nameof(MimicryRecorder.IsPrivacyAccepted)))) + offset;
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Call, AccessTools.Method(typeof(CursedPlayer), nameof(CursedPlayer.Get), new[] { typeof(ReferenceHub) })),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerSavingVoiceEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp939EventsHandler), nameof(Scp939EventsHandler.OnPlayerSavingVoice))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerSavingVoiceEventArgs), nameof(PlayerSavingVoiceEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, retLabel),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
