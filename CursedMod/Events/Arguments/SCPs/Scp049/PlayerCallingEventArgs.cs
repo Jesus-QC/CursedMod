c8a676e825115cd7b90c699068255508c1f80060
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-07 03:19:08 +0200

    Finish all Scp049 Events

---
+++ CursedMod/Events/Arguments/SCPs/Scp049/PlayerCallingEventArgs.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerCallingEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+using PlayerRoles.PlayableScps.Scp049;
+
+namespace CursedMod.Events.Arguments.SCPs.Scp049;
+
+public class PlayerCallingEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent
+{
+    public PlayerCallingEventArgs(Scp049CallAbility callAbility)
+    {
+        IsAllowed = true;
+        Player = CursedPlayer.Get(callAbility.Owner);
+    }
+    
+    public bool IsAllowed { get; set; }
+
+    public CursedPlayer Player { get; }
+}
