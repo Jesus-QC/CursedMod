5f99155838ca5fba998b82b709abf8c9f97996b1
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-07 05:16:56 +0200

    Bloodlusting

---
+++ CursedMod/Events/Arguments/SCPs/Scp0492/PlayerBloodlustingEventArgs.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerBloodlustingEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+
+namespace CursedMod.Events.Arguments.SCPs.Scp0492;
+
+public class PlayerBloodlustingEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent
+{
+    public PlayerBloodlustingEventArgs(CursedPlayer player, CursedPlayer target)
+    {
+        IsAllowed = true;
+        Player = player;
+        Target = target;
+    }
+    
+    public bool IsAllowed { get; set; }
+
+    public CursedPlayer Player { get; }
+    
+    public CursedPlayer Target { get; }
+}
