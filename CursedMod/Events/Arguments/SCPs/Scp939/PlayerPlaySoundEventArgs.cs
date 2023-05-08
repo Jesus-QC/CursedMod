024d47845e069d4c5c44f435eb5e057150df950d
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-08 17:43:44 +0200

    MimicPoint Events, Sounds, Voices and Lunge

---
+++ CursedMod/Events/Arguments/SCPs/Scp939/PlayerPlaySoundEventArgs.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerPlaySoundEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+using PlayerRoles.PlayableScps.Scp939.Mimicry;
+
+namespace CursedMod.Events.Arguments.SCPs.Scp939;
+
+public class PlayerPlaySoundEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent
+{
+    public PlayerPlaySoundEventArgs(EnvironmentalMimicry environmentalMimicry)
+    {
+        IsAllowed = true;
+        Player = CursedPlayer.Get(environmentalMimicry.Owner);
+        Option = environmentalMimicry._syncSound;
+        Category = environmentalMimicry._syncCat;
+    }
+    
+    public bool IsAllowed { get; set; }
+
+    public CursedPlayer Player { get; }
+    
+    public byte Option { get; }
+    
+    public byte Category { get; }
+}
---
+++ CursedMod/Events/Arguments/SCPs/Scp939/PlayerRemoveMimicPointEventArgs.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerRemoveMimicPointEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+using PlayerRoles.PlayableScps.Scp939.Mimicry;
+
+namespace CursedMod.Events.Arguments.SCPs.Scp939;
+
+public class PlayerRemoveMimicPointEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent
+{
+    public PlayerRemoveMimicPointEventArgs(MimicPointController mimicPointController)
+    {
+        IsAllowed = true;
+        Player = CursedPlayer.Get(mimicPointController.Owner);
+    }
+    
+    public bool IsAllowed { get; set; }
+
+    public CursedPlayer Player { get; }
+}
