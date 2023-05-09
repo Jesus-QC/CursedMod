5e12b3043a477f64f57a4b9517b0ebe160dfe72f
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-09 04:40:13 +0200

    Enrage Events and more

---
+++ CursedMod/Events/Arguments/SCPs/Scp096/PlayerEnragingEventArgs.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerEnragingEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+using PlayerRoles.PlayableScps.Scp096;
+
+namespace CursedMod.Events.Arguments.SCPs.Scp096;
+
+public class PlayerEnragingEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent
+{
+    public PlayerEnragingEventArgs(Scp096RageManager rageManager, float rageTime)
+    {
+        IsAllowed = true;
+        Player = CursedPlayer.Get(rageManager.Owner);
+        RageTime = rageTime;
+    }
+    
+    public bool IsAllowed { get; set; }
+
+    public CursedPlayer Player { get; }
+    
+    public float RageTime { get; set; }
+}
