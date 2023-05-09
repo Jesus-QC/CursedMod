03d62f70c27d8c03eaf6109ce331d4bf69584523
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-09 14:36:22 +0200

    All 096 events

---
+++ CursedMod/Events/Arguments/SCPs/Scp096/PlayerRemoveTargetEventArgs.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerRemoveTargetEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+using PlayerRoles.PlayableScps.Scp096;
+
+namespace CursedMod.Events.Arguments.SCPs.Scp096;
+
+public class PlayerRemoveTargetEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent
+{
+    public PlayerRemoveTargetEventArgs(Scp096TargetsTracker targetsTracker, CursedPlayer target)
+    {
+        IsAllowed = true;
+        Player = CursedPlayer.Get(targetsTracker.Owner);
+        Target = target;
+    }
+    
+    public bool IsAllowed { get; set; }
+
+    public CursedPlayer Player { get; }
+    
+    public CursedPlayer Target { get; }
+}
