bcc1325166899ed93a645f6d7cf120529f5f1830
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-09 14:36:03 +0200

    Finished all 096 patches

---
+++ CursedMod/Events/Patches/SCPs/Scp096/AddTargetPatch.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AddTargetPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp096;
+using CursedMod.Events.Handlers.SCPs.Scp096;
+using CursedMod.Features.Wrappers.Player;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp096;
+
+namespace CursedMod.Events.Patches.SCPs.Scp096;
+
+[DynamicEventPatch(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.PlayerAddTarget))]
+[HarmonyPatch(typeof(Scp096TargetsTracker), nameof(Scp096TargetsTracker.AddTarget))]
+public class AddTargetPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<AddTargetPatch>(70, instructions);
+
+        Label retLabel = generator.DefineLabel();
+        const int offset = 1;
+        int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Ret) + offset;
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Call, AccessTools.Method(typeof(CursedPlayer), nameof(CursedPlayer.Get), new[] { typeof(ReferenceHub) })),
+            new (OpCodes.Ldarg_2),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerAddTargetEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.OnPlayerAddTarget))),
+            new (OpCodes.Call, AccessTools.PropertyGetter(typeof(PlayerAddTargetEventArgs), nameof(PlayerAddTargetEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, retLabel),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
---
+++ CursedMod/Events/Patches/SCPs/Scp096/RemoveTargetPatch.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RemoveTargetPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp096;
+using CursedMod.Events.Handlers.SCPs.Scp096;
+using CursedMod.Features.Wrappers.Player;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp096;
+
+namespace CursedMod.Events.Patches.SCPs.Scp096;
+
+[DynamicEventPatch(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.PlayerRemoveTarget))]
+[HarmonyPatch(typeof(Scp096TargetsTracker), nameof(Scp096TargetsTracker.RemoveTarget))]
+public class RemoveTargetPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<RemoveTargetPatch>(37, instructions);
+
+        Label retLabel = generator.DefineLabel();
+        const int offset = 1;
+        int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Ret) + offset;
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Call, AccessTools.Method(typeof(CursedPlayer), nameof(CursedPlayer.Get), new[] { typeof(ReferenceHub) })),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerRemoveTargetEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.OnPlayerRemoveTarget))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerRemoveTargetEventArgs), nameof(PlayerRemoveTargetEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, retLabel),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
