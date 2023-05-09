37c556c73819616eab7a8a8f33f6653703accfc8
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-09 04:41:01 +0200

    patches

---
+++ CursedMod/Events/Patches/SCPs/Scp096/EndEnragePatch.cs
@@ -0,0 +1,54 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EndEnragePatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp096;
+using CursedMod.Events.Handlers.SCPs.Scp096;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp096;
+
+namespace CursedMod.Events.Patches.SCPs.Scp096;
+
+[DynamicEventPatch(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.PlayerEndEnrage))]
+[HarmonyPatch(typeof(Scp096RageManager), nameof(Scp096RageManager.ServerEndEnrage))]
+public class EndEnragePatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<EndEnragePatch>(17, instructions);
+
+        Label retLabel = generator.DefineLabel();
+        LocalBuilder localBuilder = generator.DeclareLocal(typeof(PlayerEndEnrageEventArgs));
+        int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Ret);
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new (OpCodes.Ldarg_0),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerEndEnrageEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Dup),
+            new (OpCodes.Stloc_S, localBuilder.LocalIndex),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.OnPlayerEndEnrage))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerEndEnrageEventArgs), nameof(PlayerEndEnrageEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, retLabel),
+            new (OpCodes.Ldloc_S, localBuilder.LocalIndex),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerEndEnrageEventArgs), nameof(PlayerEndEnrageEventArgs.ClearTime))),
+            new (OpCodes.Starg_S, 1),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
---
+++ CursedMod/Events/Patches/SCPs/Scp096/EnragingPatch.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EnragingPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp096;
+using CursedMod.Events.Handlers.SCPs.Scp096;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp096;
+
+namespace CursedMod.Events.Patches.SCPs.Scp096;
+
+[DynamicEventPatch(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.PlayerEnraging))]
+[HarmonyPatch(typeof(Scp096RageManager), nameof(Scp096RageManager.ServerEnrage))]
+public class EnragingPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<EnragingPatch>(46, instructions);
+        
+        Label retLabel = generator.DefineLabel();
+        LocalBuilder args = generator.DeclareLocal(typeof(PlayerEnragingEventArgs));
+
+        int index = newInstructions.FindIndex(i => i.IsLdarg(0));
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerEnragingEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Dup),
+            new (OpCodes.Stloc_S, args.LocalIndex),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.OnPlayerEnraging))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerEnragingEventArgs), nameof(PlayerEnragingEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, retLabel),
+            new (OpCodes.Ldloc_S, args.LocalIndex),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerEnragingEventArgs), nameof(PlayerEnragingEventArgs.RageTime))),
+            new (OpCodes.Starg_S, 1),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
