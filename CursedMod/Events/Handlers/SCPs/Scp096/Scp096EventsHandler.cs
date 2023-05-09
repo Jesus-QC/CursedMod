42012c4b4c94b68e8f47473c5fcadceba963f1c8
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-09 04:40:41 +0200

    handlers

--- CursedMod/Events/Handlers/SCPs/Scp096/Scp096EventsHandler.cs
+++ CursedMod/Events/Handlers/SCPs/Scp096/Scp096EventsHandler.cs
@@ -14,7 +14,13 @@ public static class Scp096EventsHandler
 {
     public static event EventManager.CursedEventHandler<PlayerPryGateEventArgs> PlayerPryGate;
     
-    public static event EventManager.CursedEventHandler<PlayerTryNotToCryEventArgs> PlayerTryNotToCry; 
+    public static event EventManager.CursedEventHandler<PlayerTryNotToCryEventArgs> PlayerTryNotToCry;
+    
+    public static event EventManager.CursedEventHandler<PlayerChargingEventArgs> PlayerCharging;
+    
+    public static event EventManager.CursedEventHandler<PlayerEnragingEventArgs> PlayerEnraging;
+    
+    public static event EventManager.CursedEventHandler<PlayerEndEnrageEventArgs> PlayerEndEnrage; 
 
     internal static void OnPlayerPryGate(PlayerPryGateEventArgs ev)
     {
@@ -24,5 +30,20 @@ public static class Scp096EventsHandler
     internal static void OnPlayerTryNotToCry(PlayerTryNotToCryEventArgs ev)
     {
         PlayerTryNotToCry.InvokeEvent(ev);
+    }
+    
+    internal static void OnPlayerCharging(PlayerChargingEventArgs ev)
+    {
+        PlayerCharging.InvokeEvent(ev);
+    }
+    
+    internal static void OnPlayerEnraging(PlayerEnragingEventArgs ev)
+    {
+        PlayerEnraging.InvokeEvent(ev);
+    }
+    
+    internal static void OnPlayerEndEnrage(PlayerEndEnrageEventArgs ev)
+    {
+        PlayerEndEnrage.InvokeEvent(ev);
     }
 }
