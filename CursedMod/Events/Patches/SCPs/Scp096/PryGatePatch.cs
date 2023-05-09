56a15301fc12b8769757a6381f03a11892ced1a8
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-09 02:18:36 +0200

    First patches for 096

---
+++ CursedMod/Events/Patches/SCPs/Scp096/PryGatePatch.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PryGatePatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp096;
+using CursedMod.Events.Handlers.SCPs.Scp096;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp096;
+
+namespace CursedMod.Events.Patches.SCPs.Scp096;
+
+[DynamicEventPatch(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.PlayerPryGate))]
+[HarmonyPatch(typeof(Scp096PrygateAbility), nameof(Scp096PrygateAbility.ServerProcessCmd))]
+public class PryGatePatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<PryGatePatch>(70, instructions);
+        
+        Label returnLabel = generator.DefineLabel();
+        const int offset = 1;
+        int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Stfld) + offset;
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new (OpCodes.Ldarg_0),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerPryGateEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.OnPlayerPryGate))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerPryGateEventArgs), nameof(PlayerPryGateEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, returnLabel),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
---
+++ CursedMod/Events/Patches/SCPs/Scp096/UseTryNotToCryPatch.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UseTryNotToCryPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp096;
+using CursedMod.Events.Handlers.SCPs.Scp096;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp096;
+
+namespace CursedMod.Events.Patches.SCPs.Scp096;
+
+[DynamicEventPatch(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.PlayerTryNotToCry))]
+[HarmonyPatch(typeof(Scp096TryNotToCryAbility), nameof(Scp096TryNotToCryAbility.ServerProcessCmd))]
+public class UseTryNotToCryPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<UseTryNotToCryPatch>(25, instructions);
+        
+        Label retLabel = generator.DefineLabel();
+        const int offset = 1;
+        int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ret) + offset;
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerTryNotToCryEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp096EventsHandler), nameof(Scp096EventsHandler.OnPlayerTryNotToCry))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerTryNotToCryEventArgs), nameof(PlayerTryNotToCryEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, retLabel),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
