8d00e36a68f5e8278cbe03a9529daaa2585757f1
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-10 22:09:30 +0200

    Scp079 Handlers

---
+++ CursedMod/Events/Handlers/SCPs/Scp079/Scp079EventsHandler.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Scp079EventsHandler.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using CursedMod.Events.Arguments.SCPs.Scp079;
+
+namespace CursedMod.Events.Handlers.SCPs.Scp079;
+
+public static class Scp079EventsHandler
+{
+    public static event EventManager.CursedEventHandler<PlayerBlackoutRoomEventArgs> PlayerBlackoutRoom;
+    
+    public static event EventManager.CursedEventHandler<PlayerBlackoutZoneEventArgs> PlayerBlackoutZone;
+    
+    public static event EventManager.CursedEventHandler<PlayerChangeCameraEventArgs> PlayerChangeCamera;
+    
+    public static event EventManager.CursedEventHandler<PlayerMoveElevatorEventArgs> PlayerMoveElevator;
+    
+    public static event EventManager.CursedEventHandler<PlayerUseLockdownEventArgs> PlayerUseLockdown; 
+
+    internal static void OnPlayerBlackoutRoom(PlayerBlackoutRoomEventArgs args)
+    {
+        PlayerBlackoutRoom.InvokeEvent(args);
+    }
+    
+    internal static void OnPlayerBlackoutZone(PlayerBlackoutZoneEventArgs args)
+    {
+        PlayerBlackoutZone.InvokeEvent(args);
+    }
+    
+    internal static void OnPlayerChangeCamera(PlayerChangeCameraEventArgs args)
+    {
+        PlayerChangeCamera.InvokeEvent(args);
+    }
+    
+    internal static void OnPlayerMoveElevator(PlayerMoveElevatorEventArgs args)
+    {
+        PlayerMoveElevator.InvokeEvent(args);
+    }
+    
+    internal static void OnPlayerUseLockdown(PlayerUseLockdownEventArgs args)
+    {
+        PlayerUseLockdown.InvokeEvent(args);
+    }
+}
