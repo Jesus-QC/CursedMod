0fbd494ad9f82d1f6d6a2c56a43a806b02b34733
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-10 22:09:38 +0200

    Scp079 Patches

---
+++ CursedMod/Events/Patches/SCPs/Scp079/BlackoutZoneAbilityPatch.cs
@@ -0,0 +1,58 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BlackoutZoneAbilityPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp079;
+using CursedMod.Events.Handlers.SCPs.Scp079;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp079;
+
+namespace CursedMod.Events.Patches.SCPs.Scp079;
+
+[DynamicEventPatch(typeof(Scp079EventsHandler), nameof(Scp079EventsHandler.PlayerBlackoutZone))]
+[HarmonyPatch(typeof(Scp079BlackoutZoneAbility), nameof(Scp079BlackoutZoneAbility.ServerProcessCmd))]
+public class BlackoutZoneAbilityPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<BlackoutZoneAbilityPatch>(71, instructions);
+        
+        const int offset = 4;
+        int index = newInstructions.FindIndex(
+            i => i.LoadsField(AccessTools.Field(typeof(Scp079BlackoutZoneAbility), nameof(Scp079BlackoutZoneAbility._syncZone)))) + offset;
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new (OpCodes.Pop),
+            new (OpCodes.Ldarg_0),
+            new (OpCodes.Call, AccessTools.Method(typeof(BlackoutZoneAbilityPatch), nameof(ProcessBlackoutZoneEvent))),
+        });
+
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+
+    private static bool ProcessBlackoutZoneEvent(Scp079BlackoutZoneAbility blackoutZoneAbility)
+    {
+        PlayerBlackoutZoneEventArgs args = new (blackoutZoneAbility, blackoutZoneAbility._cost, blackoutZoneAbility._duration);
+        Scp079EventsHandler.OnPlayerBlackoutZone(args);
+        
+        if (args.IsAllowed)
+        {
+            blackoutZoneAbility._duration = args.Duration;
+            blackoutZoneAbility._cooldownTimer.Trigger(blackoutZoneAbility._cooldown);
+            blackoutZoneAbility._cost = args.PowerCost;
+        }
+        
+        return args.IsAllowed;
+    }
+}
---
+++ CursedMod/Events/Patches/SCPs/Scp079/ElevatorStateChangerPatch.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ElevatorStateChangerPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp079;
+using CursedMod.Events.Handlers.SCPs.Scp079;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp079;
+using PlayerRoles.PlayableScps.Scp079.Cameras;
+
+namespace CursedMod.Events.Patches.SCPs.Scp079;
+
+[DynamicEventPatch(typeof(Scp079EventsHandler), nameof(Scp079EventsHandler.PlayerMoveElevator))]
+[HarmonyPatch(typeof(Scp079ElevatorStateChanger), nameof(Scp079ElevatorStateChanger.ServerProcessCmd))]
+public class ElevatorStateChangerPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<ElevatorStateChangerPatch>(104, instructions);
+        
+        Label returnLabel = generator.DefineLabel();
+
+        const int offset = -2;
+        int index = newInstructions.FindIndex(i => i.opcode == OpCodes.Stloc_S) + offset;
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+            new (OpCodes.Ldloc_3),
+            new (OpCodes.Ldarg_0),
+            new (OpCodes.Call, AccessTools.PropertyGetter(typeof(Scp079AbilityBase), nameof(Scp079AbilityBase.CurrentCamSync))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(Scp079CurrentCameraSync), nameof(Scp079CurrentCameraSync.CurrentCamera))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(Scp079Camera), nameof(Scp079Camera.Room))),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerMoveElevatorEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp079EventsHandler), nameof(Scp079EventsHandler.OnPlayerMoveElevator))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerMoveElevatorEventArgs), nameof(PlayerMoveElevatorEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, returnLabel),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
