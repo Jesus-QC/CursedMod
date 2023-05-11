e785d50df2e7ad0bca6f6b103f8af1c99272e7bc
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-11 02:16:26 +0200

    Leveling Patches

---
+++ CursedMod/Events/Patches/SCPs/Scp079/GainExperiencePatch.cs
@@ -0,0 +1,57 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GainExperiencePatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp079;
+using CursedMod.Events.Handlers.SCPs.Scp079;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp079;
+
+namespace CursedMod.Events.Patches.SCPs.Scp079;
+
+[DynamicEventPatch(typeof(Scp079EventsHandler), nameof(Scp079EventsHandler.PlayerGainExperience))]
+[HarmonyPatch(typeof(Scp079TierManager), nameof(Scp079TierManager.ServerGrantExperience))]
+public class GainExperiencePatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<GainExperiencePatch>(22, instructions);
+
+        LocalBuilder args = generator.DeclareLocal(typeof(PlayerGainExperienceEventArgs));
+        Label returnLabel = generator.DefineLabel();
+        
+        newInstructions.InsertRange(0, new CodeInstruction[]
+        {
+            new (OpCodes.Ldarg_0),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Ldarg_2),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerGainExperienceEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Dup),
+            new (OpCodes.Stloc_S, args.LocalIndex),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp079EventsHandler), nameof(Scp079EventsHandler.OnPlayerGainExperience))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerGainExperienceEventArgs), nameof(PlayerGainExperienceEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, returnLabel),
+            new (OpCodes.Ldloc_S, args.LocalIndex),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerGainExperienceEventArgs), nameof(PlayerGainExperienceEventArgs.Experience))),
+            new (OpCodes.Starg_S, 1),
+            new (OpCodes.Ldloc_S, args.LocalIndex),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerGainExperienceEventArgs), nameof(PlayerGainExperienceEventArgs.HudTranslation))),
+            new (OpCodes.Starg_S, 2),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
---
+++ CursedMod/Events/Patches/SCPs/Scp079/GainLevelPatch.cs
@@ -0,0 +1,60 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GainLevelPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp079;
+using CursedMod.Events.Handlers.SCPs.Scp079;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp079;
+
+namespace CursedMod.Events.Patches.SCPs.Scp079;
+
+[DynamicEventPatch(typeof(Scp079EventsHandler), nameof(Scp079EventsHandler.PlayerLevelUp))]
+[HarmonyPatch(typeof(Scp079TierManager), nameof(Scp079TierManager.AccessTierIndex), MethodType.Setter)]
+public class GainLevelPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<GainLevelPatch>(62, instructions);
+
+        const int offset = 1;
+        int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ret) + offset;
+
+        Label returnLabel = generator.DefineLabel();
+        LocalBuilder args = generator.DeclareLocal(typeof(PlayerLevelUpEventArgs));
+
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Ldc_I4_1),
+            new (OpCodes.Add),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerLevelUpEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Dup),
+            new (OpCodes.Stloc_S, args.LocalIndex),
+            new (OpCodes.Call, AccessTools.Method(typeof(Scp079EventsHandler), nameof(Scp079EventsHandler.OnPlayerLevelUp))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerLevelUpEventArgs), nameof(PlayerLevelUpEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, returnLabel),
+            new (OpCodes.Ldloc_S, args.LocalIndex),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerLevelUpEventArgs), nameof(PlayerLevelUpEventArgs.Level))),
+            new (OpCodes.Ldc_I4_1),
+            new (OpCodes.Sub),
+            new (OpCodes.Starg_S, 1),
+        });
+        
+        newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
