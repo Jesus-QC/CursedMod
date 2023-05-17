c72087e3e5e08f012f4c5063c371a78f931e4852
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-17 03:45:25 +0200

    Patch

---
+++ CursedMod/Events/Patches/SCPs/Scp079/PingPatch.cs
@@ -0,0 +1,50 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PingPatch.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using CursedMod.Events.Arguments.SCPs.Scp079;
+using CursedMod.Events.Handlers;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles.PlayableScps.Scp079.Pinging;
+using RelativePositioning;
+
+namespace CursedMod.Events.Patches.SCPs.Scp079;
+
+[DynamicEventPatch(typeof(CursedScp079EventsHandler), nameof(CursedScp079EventsHandler.PlayerPing))]
+[HarmonyPatch(typeof(Scp079PingAbility), nameof(Scp079PingAbility.ServerProcessCmd))]
+public class PingPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = EventManager.CheckEvent<PingPatch>(53, instructions);
+        
+        Label returnLabel = generator.DefineLabel();
+        const int offset = -2;
+        int index = newInstructions.FindIndex(
+            instruction => instruction.Calls(AccessTools.Method(typeof(RelativePositionSerialization), nameof(RelativePositionSerialization.ReadRelativePosition)))) + offset;
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+            new (OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(PlayerPingEventArgs))[0]),
+            new (OpCodes.Dup),
+            new (OpCodes.Call, AccessTools.Method(typeof(CursedScp079EventsHandler), nameof(CursedScp079EventsHandler.OnPlayerPing))),
+            new (OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerPingEventArgs), nameof(PlayerPingEventArgs.IsAllowed))),
+            new (OpCodes.Brfalse_S, returnLabel),
+        });
+        
+        newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+        
+        foreach (var instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
