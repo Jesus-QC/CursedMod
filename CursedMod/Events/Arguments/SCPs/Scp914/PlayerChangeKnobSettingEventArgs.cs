714a3196777c37ffe8d17aa392cc4549df363258
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-17 05:33:55 +0200

    First 3 Scp914 events

---
+++ CursedMod/Events/Arguments/SCPs/Scp914/PlayerChangeKnobSettingEventArgs.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerChangeKnobSettingEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+using Scp914;
+
+namespace CursedMod.Events.Arguments.SCPs.Scp914;
+
+public class PlayerChangeKnobSettingEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent
+{
+    public PlayerChangeKnobSettingEventArgs(CursedPlayer player, Scp914KnobSetting knobSetting)
+    {
+        IsAllowed = true;
+        Player = player;
+        KnobSetting = knobSetting;
+    }
+    
+    public bool IsAllowed { get; set; }
+
+    public CursedPlayer Player { get; }
+    
+    public Scp914KnobSetting KnobSetting { get; set; }
+}
---
+++ CursedMod/Events/Arguments/SCPs/Scp914/PlayerUpgradeItemEventArgs.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerUpgradeItemEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using CursedMod.Features.Wrappers.Inventory.Pickups;
+using CursedMod.Features.Wrappers.Player;
+using Scp914;
+using UnityEngine;
+
+namespace CursedMod.Events.Arguments.SCPs.Scp914;
+
+public class PlayerUpgradeItemEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent, ICursedPickupEvent
+{
+    public PlayerUpgradeItemEventArgs(CursedPlayer player, CursedPickup pickup, List<CursedPickup> pickups, Vector3 outputPosition, Scp914KnobSetting knobSetting)
+    {
+        IsAllowed = true;
+        Player = player;
+        Pickup = pickup;
+        PickupsToUpgrade = pickups;
+        OutputPosition = outputPosition;
+        KnobSetting = knobSetting;
+    }
+
+    public bool IsAllowed { get; set; }
+
+    public CursedPickup Pickup { get; }
+    
+    public List<CursedPickup> PickupsToUpgrade { get; }
+
+    public CursedPlayer Player { get; }
+    
+    public Vector3 OutputPosition { get; }
+    
+    public Scp914KnobSetting KnobSetting { get; }
+}
