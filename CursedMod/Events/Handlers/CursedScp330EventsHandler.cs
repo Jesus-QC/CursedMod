2d079ea66a3404ac3d902f3acffe62a2bdb4ed58
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-17 22:30:24 +0200

    Eat Handler

--- CursedMod/Events/Handlers/CursedScp330EventsHandler.cs
+++ CursedMod/Events/Handlers/CursedScp330EventsHandler.cs
@@ -13,9 +13,16 @@ namespace CursedMod.Events.Handlers;
 public static class CursedScp330EventsHandler
 {
     public static event EventManager.CursedEventHandler<PlayerInteractingScp330EventArgs> PlayerInteractingScp330;
+    
+    public static event EventManager.CursedEventHandler<PlayerEatScp330EventArgs> PlayerEatScp330; 
 
     internal static void OnPlayerInteractingScp330(PlayerInteractingScp330EventArgs args)
     {
         PlayerInteractingScp330.InvokeEvent(args);
+    }
+    
+    internal static void OnPlayerEatScp330(PlayerEatScp330EventArgs args)
+    {
+        PlayerEatScp330.InvokeEvent(args);
     }
 }
