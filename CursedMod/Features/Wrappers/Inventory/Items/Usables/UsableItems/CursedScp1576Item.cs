6e2acae3d02e0f07141ab6456cc55233c68d151d
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-20 01:51:39 +0200

    Some SCP Usable Items

---
+++ CursedMod/Features/Wrappers/Inventory/Items/Usables/UsableItems/CursedScp1576Item.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CursedScp1576Item.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using InventorySystem.Items.Usables.Scp1576;
+using UnityEngine;
+
+namespace CursedMod.Features.Wrappers.Inventory.Items.Usables.UsableItems;
+
+public class CursedScp1576Item : CursedUsableItem
+{
+    public CursedScp1576Item(Scp1576Item scp1576Item)
+        : base(scp1576Item)
+    {
+        Scp1576Base = scp1576Item;
+    }
+    
+    public Scp1576Item Scp1576Base { get; }
+    
+    public Scp1576Playback PlaybackTemplate => Scp1576Base.PlaybackTemplate;
+
+    public void RevalidateReceivers() => Scp1576Item.RevalidateReceivers();
+
+    public void PlayWarningSound(AudioClip clip) => Scp1576Item.PlayWarningSound(clip);
+
+    public void StopTransmission() => Scp1576Base.ServerStopTransmitting();
+}
