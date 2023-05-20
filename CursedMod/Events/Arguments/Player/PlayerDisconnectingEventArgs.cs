34af990d66b95bf10032ac7492ba644e0ba1f26a
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2023-05-20 22:08:00 +0200

    PlayerDisconnected + Disconnecting

---
+++ CursedMod/Events/Arguments/Player/PlayerDisconnectingEventArgs.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerDisconnectingEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+using Mirror;
+
+namespace CursedMod.Events.Arguments.Player;
+
+public class PlayerDisconnectingEventArgs : PlayerDisconnectedEventArgs
+{
+    public PlayerDisconnectingEventArgs(NetworkConnection connection)
+        : base(connection)
+    {
+    }
+}
