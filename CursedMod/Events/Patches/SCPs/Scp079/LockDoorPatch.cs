6a172e5ac1c64a555817a3d7ed3f6ca0454fffe0
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-06-23 06:41:05 +0200

    this patch need to be redone

--- CursedMod/Events/Patches/SCPs/Scp079/LockDoorPatch.cs
+++ CursedMod/Events/Patches/SCPs/Scp079/LockDoorPatch.cs
@@ -17,11 +17,11 @@ using PlayerRoles.PlayableScps.Scp079;
 
 namespace CursedMod.Events.Patches.SCPs.Scp079;
 
-[DynamicEventPatch(typeof(CursedScp079EventsHandler), nameof(CursedScp079EventsHandler.ChangingDoorLock))]
-[HarmonyPatch(typeof(Scp079DoorLockChanger), nameof(Scp079DoorLockChanger.SetDoorLock))]
+// [DynamicEventPatch(typeof(CursedScp079EventsHandler), nameof(CursedScp079EventsHandler.ChangingDoorLock))]
+// [HarmonyPatch(typeof(Scp079DoorLockChanger), nameof(Scp079DoorLockChanger.SetDoorLock))]
 public class LockDoorPatch
 {
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    /*private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
     {
         List<CodeInstruction> newInstructions = CursedEventManager.CheckEvent<LockDoorPatch>(35, instructions);
         
@@ -59,5 +59,5 @@ public class LockDoorPatch
             yield return instruction;
         
         ListPool<CodeInstruction>.Shared.Return(newInstructions);
-    }
+    }*/
 }
