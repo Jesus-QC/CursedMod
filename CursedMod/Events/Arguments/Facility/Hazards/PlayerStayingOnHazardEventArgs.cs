45c7f2ccd396cc2d9938207aa5c0cdf4769031de
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-07-12 13:36:00 +0200

    EventArgs

---
+++ CursedMod/Events/Arguments/Facility/Hazards/PlayerStayingOnHazardEventArgs.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerStayingOnHazardEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Facility.Hazards;
+using CursedMod.Features.Wrappers.Player;
+using Hazards;
+
+namespace CursedMod.Events.Arguments.Facility.Hazards;
+
+public class PlayerStayingOnHazardEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent, ICursedHazardEvent
+{
+    public PlayerStayingOnHazardEventArgs(ReferenceHub player, EnvironmentalHazard hazard)
+    {
+        Player = CursedPlayer.Get(player);
+        Hazard = CursedEnvironmentalHazard.Get(hazard);
+        IsAllowed = true;
+    }
+    
+    public CursedPlayer Player { get; }
+
+    public CursedEnvironmentalHazard Hazard { get; }
+
+    public bool IsAllowed { get; set; }
+}
