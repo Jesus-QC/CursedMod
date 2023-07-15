936eaef315a6561f7120fecb661beb4acaca9c5b
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-07-15 20:27:46 +0200

    BulletHole + BloodDecal args

---
+++ CursedMod/Events/Arguments/Player/PlayerPlacingBloodDecalEventArgs.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerPlacingBloodDecalEventArgs.cs" company="CursedMod">
+// Copyright (c) CursedMod. All rights reserved.
+// Licensed under the GPLv3 license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using CursedMod.Features.Wrappers.Player;
+using UnityEngine;
+
+namespace CursedMod.Events.Arguments.Player;
+
+public class PlayerPlacingBloodDecalEventArgs : EventArgs, ICursedCancellableEvent, ICursedPlayerEvent
+{
+    public PlayerPlacingBloodDecalEventArgs(ReferenceHub player, ReferenceHub target, Vector3 pos)
+    {
+        Player = CursedPlayer.Get(player);
+        Target = CursedPlayer.Get(target);
+        Position = pos;
+        IsAllowed = true;
+    }
+    
+    public CursedPlayer Player { get; }
+    
+    public CursedPlayer Target { get; }
+    
+    public Vector3 Position { get; }
+
+    public bool IsAllowed { get; set; }
+}
